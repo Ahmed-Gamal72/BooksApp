8b9f93756213785060232279a34c45506d108930
Ahmed-Gamal72 <ahmdgma.97@gmail.com>
2021-02-24 15:59:09 +0200

    first commit

---
+++ Books/Models/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Books.Models
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext():base("DefaultConnection")
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+
+    }
+}
